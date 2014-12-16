4ee8c94ec3248dc1c3033f0db20f5263803a1ce9
amoni13 <amoni13@users.noreply.github.com>
2014-12-16 13:57:19 -0500

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -5,10 +5,11 @@ data<-rbind(read.table("test/X_test.txt"),read.table("train/X_train.txt"))
 
 ###From merged dataset selects only calculated mean/std measurements###
 meanstd<-grep("*mean\\(\\)|*std\\(\\)", names(data), ignore.case=TRUE)
-    ##excludes measuremend on MeanFreq() and gravityMean or where Mean is used as part of additional function
+    ##excludes measurements on MeanFreq() and gravityMean or where Mean is used as part of additional function
 data_step2<-data[,sort(meanstd)]
+    ##preserves original order of measurements while limiting to desired mean/std variables
 
-###Adds participant and activity information with descriptive variable names###
+###Adds participant and activity information to data set###
     activity<-rbind(read.table("test/y_test.txt"), read.table("train/y_train.txt"))
         colnames(activity)<-"activity"
     participant<-rbind(read.table("test/subject_test.txt"),read.table("train/subject_train.txt"))
@@ -21,10 +22,11 @@ data_step3<-cbind(participant, activity, data_step2)
 data_step3$activity<-factor(data_step3$activity, levels=recode, labels=names(recode))
 
 ###Creates new tidy dataset with average of each variable for each activity and subject###
-library(dplyr)
+require(dplyr)
+    #will load dplyr package, will first install package if not already available in library
 data_step4<-group_by(data_step3, activity, participant)
 data_step4<-summarise_each(data_step4, funs(mean))
 
-###Creates and saves tidy data as .txt file
+###Creates and saves tidy data as .txt file to working directory
 write.table(data_step4, "tidy_data.txt", row.names=FALSE)
 
